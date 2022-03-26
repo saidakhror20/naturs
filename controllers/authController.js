f0015cb161483edf2d4672093cb05bbb3d2be7da
saidakhror20 <saidakhrorkhonabdukodirov@gmail.com>
2022-03-26 20:43:34 +0500

    jwt added to cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,11 +14,18 @@ const createToken = id =>{
 
 const createSendToken = (user,statusCode, res)=>{
     const token = createToken(user._id)
-        res.status(statusCode).json({
-            status:'success',
-            token,
-            user   
-        })
+    let cookieOptions = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIES_EXPIRES_IN * 24 * 60 * 60 * 1000),
+        httpOnly:true
+    }
+    if(process.env.NODE_ENV === 'production') cookieOptions.secure = true
+    res.cookie('jwt', token, cookieOptions)
+    user.password = undefined;
+    res.status(statusCode).json({
+        status:'success',
+        token,
+        user   
+    })
 }
 exports.signup = catchAsync(
     async (req,res,next)=>{
