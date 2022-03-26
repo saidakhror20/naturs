e6cb5e25b4896e5006e8bd75b4fdc4197af63bc1
saidakhror20 <saidakhrorkhonabdukodirov@gmail.com>
2022-03-26 20:50:40 +0500

    rate limiter set to 100

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const app = express()
+const rateLimit = require('express-rate-limit')
 const tourRouter = require('./routes/toursRouter')
 const userRouter = require('./routes/userRouter')
 const AppError = require('./utils/appError')
 const AppErrorHander = require('./controllers/appErrorHandler')
 app.use(express.json())
-
 app.use((req, res, next)=>{
     req.currentTime = new Date().toISOString()
     next()
 })
+
+const limiter = rateLimit({
+    max:100,
+    windowMs: 60 * 60 * 1000,
+    message:"Too many requests, try later"
+})
+app.use('/api',limiter)
+
 
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
