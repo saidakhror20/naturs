5e251871f03d8aed0e48a36efd7be88c81397037
saidakhror20 <saidakhrorkhonabdukodirov@gmail.com>
2022-03-28 21:43:21 +0500

    data sanitation

--- app.js
+++ app.js
@@ -2,18 +2,32 @@ const express = require('express')
 const app = express()
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require("hpp")
 const tourRouter = require('./routes/toursRouter')
 const userRouter = require('./routes/userRouter')
 const AppError = require('./utils/appError')
 const AppErrorHander = require('./controllers/appErrorHandler')
-
+// HTTP
 app.use(helmet())
-
+// BODY PARSER
 app.use(express.json({
     limit:'10kb'
 }))
+// SANITIZE BODY AND PARAMS against noSQL query injection
+app.use(mongoSanitize());
+app.use(xss())
+// Prevent parametre pollution
+app.use(hpp({
+    whitelist:['duration']
+}))
 
+// STATIC files
 app.use(express.static(`${__dirname}/public`))
+
+
+//   
 app.use((req, res, next)=>{
     req.currentTime = new Date().toISOString()
     next()
